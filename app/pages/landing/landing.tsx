b38b4660a64a95a41e619876760eadb8b9b78cac
JeffreyYeet1 <jeffrey.ye@torontomu.ca>
2025-01-18 02:49:55 -0500

    Added framework for NavBar and landing

---
+++ app/pages/landing/landing.tsx
@@ -0,0 +1,8 @@
+export default function Landing() {
+    return(
+        <main className = "w-screen h-screen flex flex-col space-y-8 justify-center items-center">
+            <h1>Welcome to MockMate</h1>
+            <p>The best AI interview prep assistant</p>
+        </main>
+    );
+}
